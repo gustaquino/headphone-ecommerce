2f11953489f929b16eb6a42de73f9f7f157ba6e9
Gustavo de Aquino Fioranti <gustavoaquinofioranti@gmail.com>
2023-01-23 18:18:55 -0300

    fixed responsivness and styled navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { ShoppingCart } from 'phosphor-react';
+import { Storefront } from 'phosphor-react';
+import { Headphones } from 'phosphor-react';
 
 export const Navbar = () => {
   return (
-    <div className="w-full h-20 bg-black/90 flex justify-end items-center">
-      <div className="links flex items-center mr-13">
-        <Link to={'/'} className="text-white text-2xl mr-5 no-underline">
-          {' '}
-          Shop{' '}
+    <div className="w-full h-20 flex justify-end">
+      <div className="links flex items-center mr-13 px-4 justify-end">
+        <Link
+          to={'/'}
+          className=" mr-5 no-underline flex text-center border-2 border-[#ebe5f9d9] bg-[#ebe5f9d9] rounded-xl w-full justify-between text-black gap-2 px-2"
+        >
+          <p className="text-black items-center text-xl justify-center content-center flex">Store</p>
+          <Storefront size={35} weight="thin" />
         </Link>
         <Link to={'/cart'}>
           {' '}
-          <ShoppingCart size={40} className="text-white" />{' '}
+          <ShoppingCart
+            size={40}
+            className="text-white border-2 border-white py-1 px-1 bg-orange-300 rounded-xl"
+          />{' '}
         </Link>
       </div>
       ;
--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import { PRODUCTS } from '../../products';
+import { Product } from './product';
 
 export const Shop = () => {
-  return <div>shop</div>;
+  return (
+    <div className="">
+      <div className="mt-[100px] text-center text-[40px]">
+        <h1 className="font-semibold">Headphone Shop</h1>
+      </div>
+
+      <div className="products w-full h-auto flex flex-1 flex-col lg:flex-row content-center justify-center place-items-center flex-wrap">
+        {PRODUCTS.map((product) => (
+          <Product data={product} />
+        ))}
+      </div>
+    </div>
+  );
 };
