58f8dbd58359e7f589f5b26c2a748a02e8596329
Gustavo de Aquino Fioranti <gustavoaquinofioranti@gmail.com>
2023-01-25 19:11:31 -0300

    add hover effect to item buttons

---
+++ src/pages/shop/Product.jsx
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react';
+
+export const Product = (props) => {
+  const { id, productName, price, productImage } = props.data;
+
+  return (
+    <div className="product rounded-xl w-[300px] h-[350px] m-[100px] flex flex-col justify-center items-center">
+      <img src={productImage} className="w-[400px] h-auto " />
+      <div className="description text-center">
+        <p>
+          <b>{productName}</b>
+        </p>
+        <p> ${price}</p>
+      </div>
+      <button className="border-2 mt-1 border-white text-white bg-[#4C2895] rounded-2xl py-2 px-4 hover:bg-[#5d3d9f] hover:scale-108">
+        Add to Cart
+      </button>
+    </div>
+  );
+};
---
+++ src/products.js
@@ -0,0 +1,44 @@
+import applemax from './assets/applemax.jpg';
+import shd80 from './assets/shd80.jpg';
+import philco from './assets/philco.jpg';
+import hd200 from './assets/hd200.jpg';
+import montblanc from './assets/montblanc.jpg';
+import w800 from './assets/w800.jpg';
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: 'Sony cancelling',
+    price: 1900.0,
+    productImage: applemax,
+  },
+  {
+    id: 2,
+    productName: 'Sennheiser 200',
+    price: 2500.0,
+    productImage: hd200,
+  },
+  {
+    id: 3,
+    productName: 'Montblanc MB',
+    price: 4800.0,
+    productImage: montblanc,
+  },
+  {
+    id: 4,
+    productName: 'Shd 80',
+    price: 250.0,
+    productImage: shd80,
+  },
+  {
+    id: 5,
+    productName: 'Philco',
+    price: 1000.0,
+    productImage: philco,
+  },
+  {
+    id: 6,
+    productName: 'W800',
+    price: 150.0,
+    productImage: w800,
+  },
+];
