72dcc9b255a0ce7ecf8225540dbc124f7df868fd
Gustavo de Aquino Fioranti <gustavoaquinofioranti@gmail.com>
2023-02-14 17:09:08 -0300

    finished styling and features for now, gonna add checkout later

--- src/pages/cart/Cart-item.jsx
+++ src/pages/cart/Cart-item.jsx
@@ -6,23 +6,19 @@ export const CartItem = (props) => {
   const { cartItems, addToCart, removeFromCart, updateCartItemCount } = useContext(ShopContext);
 
   return (
-    <div className="cartItem rounded-xl bg-[#fff0db] relative   h-full m-[100px] flex flex-col justify-center items-center  px-2 py-2">
+    <div className="cartItem rounded-xl bg-[#fff0db] relative mt-3 h-full m-[100px] flex flex-col justify-center items-center  px-2 py-2">
       <img className="w-[400px] h-auto bg-[#eed9c4] text-center" src={productImage} />
-      <div className="description  bg-[#eed9c4]">
-        <p className="text-center text-[#5C4033]">
-          <b className="text-center">{productName}</b>
+      <div className="description w-full bg-[#eed9c4]">
+        <p className="text-center w-full text-[#5C4033]">
+          <b className="text-center w-full">{productName}</b>
         </p>
-        <p className="text-center text-[#5C4033]"> Price: ${price}</p>
-        <div className="countHandler">
-          <button className="ml-4" onClick={() => removeFromCart(id)}>
+        <p className="text-center text-[#5C4033] w-full"> Price: ${price}</p>
+        <div className="countHandler flex items-center justify-center gap-1 mb-3">
+          <button className="ml-4 bg-white hover:bg-white/90 text-black font-bold px-2 border border-black/80 rounded" onClick={() => removeFromCart(id)}>
             -
           </button>
-          <input
-            className="w-4"
-            value={cartItems[id]}
-            onChange={(e) => updateCartItemCount(Number(e.target.value), id)}
-          />
-          <button className="mr-4   " onClick={() => addToCart(id)}>
+          <input className="w-4 text-center px-1 rounded-xl" value={cartItems[id]} onChange={(e) => updateCartItemCount(Number(e.target.value), id)} />
+          <button className="mr-4 bg-white hover:bg-white/90 text-black font-bold px-2 border border-black/80 rounded" onClick={() => addToCart(id)}>
             +
           </button>
         </div>
